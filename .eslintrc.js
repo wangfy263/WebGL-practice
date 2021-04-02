1b30df36081dcfa606245623f3b73e667c241267
王飞宇 <wangfeiyu_@126.com>
2021-04-02 16:42:48 +0800

    '123'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,51 @@
 module.exports = {
-  globals: {
-  },
-  extends:  "eslint-config-sprite",
+  globals: {},
+  extends: 'eslint-config-sprite',
   plugins: ['html'],
   rules: {
-    "complexity": ["warn", 25],
+    complexity: ['warn', 25],
     'import/prefer-default-export': 'off',
+    'object-curly-spacing': 'off',
+    'no-param-reassign': 'off',
+
+    'import/first': 'off',
+    'import/named': 'error',
+    'import/namespace': 'error',
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    'prefer-promise-reject-errors': 'off',
+    'arrow-body-style': ['error', 'as-needed', { requireReturnForObjectLiteral: true }],
+    'arrow-parens': ['error', 'as-needed'],
+    'no-console': ['error', { allow: ['log', 'warn', 'error'] }],
+    'comma-dangle': 'off',
+    'max-len': ['error', 150],
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectPattern: { multiline: true },
+      },
+    ],
+    'vue/no-use-v-if-with-v-for': [
+      'off',
+      {
+        allowUsingIterationVar: false,
+      },
+    ],
+    // 'space-before-function-paren': ['error', 'never'],
+
+    // allow debugger during development only
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'linebreak-style': ["error", "unix"],
+    'linebreak-style': ['off', 'windows'],
+    'vue/no-multiple-template-root': 'off',
+    'no-eval': 'off',
+    'func-names': 'off',
+    'consistent-return': 'off',
+    'vue/no-mutating-props': 'off',
+    'vue/valid-v-for': 'off',
   },
-}
+};
--- chapter06/shadedCubeImage/app.js
+++ chapter06/shadedCubeImage/app.js
@@ -52,6 +52,11 @@ const colorCube = () => {
 
 colorCube();
 
+const image = new Image();
+image.src = '/chapter06/shadedCube/img/uv.png';// 设置图片路径
+console.log(image);
+image.onload = texture;// 图片加载成功后执行texture函数
+
 console.log(points);
 console.log(pointsToBuffer(points));
 console.log(colors);
@@ -62,6 +67,22 @@ const gl = setupWebGL(canvas);
 gl.viewport(0, 0, canvas.width, canvas.height);
 gl.clearColor(1.0, 1.0, 1.0, 1.0);
 gl.enable(gl.DEPTH_TEST);
+
+function texture() {
+  const texture = gl.createTexture(); // 创建纹理图像缓冲区
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true); // 纹理图片上下反转
+  gl.activeTexture(gl.TEXTURE0); // 激活0号纹理单元TEXTURE0
+  gl.bindTexture(gl.TEXTURE_2D, texture); // 绑定纹理缓冲区
+  // 设置纹理贴图填充方式(纹理贴图像素尺寸大于顶点绘制区域像素尺寸)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  // 设置纹理贴图填充方式(纹理贴图像素尺寸小于顶点绘制区域像素尺寸)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  // 设置纹素格式，jpg格式对应gl.RGB
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+  gl.uniform1i(u_Sampler, 0); // 纹理缓冲区单元TEXTURE0中的颜色数据传入片元着色器
+  // 进行绘制
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
 
 const vertShdr = gl.createShader(gl.VERTEX_SHADER);
 const fragShdr = gl.createShader(gl.FRAGMENT_SHADER);
